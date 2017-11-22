661e73ca97f6e47b68046d777f3c01004c4a352b
moonheejoo <jmoonmoon2@gmail.com>
2017-11-21 18:48:56 -0800

    basic skeleton of js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const IMDB_SEARCH_URL = 'http://www.theimdbapi.org/api/find/movie?';
+const NYT_SEARCH_URL = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?";
+const NYT = {
+  NAME: 'nyt',
+  API_KEY: "9b85bd3d754d4f91a251f25992575fdf",
+  BASE_URL: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?"
+}
+
+
+// $.getJSON(NYT_SEARCH_URL, options);
 
 function getDataFromApi(search, callback) {
   const query = {
@@ -10,6 +19,27 @@ function getDataFromApi(search, callback) {
     .fail(function(error) {
       console.log(error);
     })
+}
+/**
+* @param {Object} api - the name of the API whose data we'll request
+* @param {string} api.NAME - A simple string which indicates the name of the api
+* @param {string} api.API_KEY - key to access an api
+* @param {string} api.BASE_URL - the base url onto which a query will be appended
+* @param {string} q - A query for the desired api
+* @param {function} cb - function to run after the initial get request
+*/
+function getFromApi(api, q, cb) {
+  let query;
+  if (api.NAME === 'nyt') {
+    console.log('talking to the nyt!!');
+     query = {
+      'api-key': api.API_KEY,
+      'query': q,
+    }
+  }
+
+
+  $.getJSON(api.BASE_URL, query, cb);
 }
 
 
@@ -36,3 +66,7 @@ $('.js-search-form').on('submit', event => {
   queryTarget.val("");
   getDataFromApi(query, cleanMovieData);
 });
+
+getFromApi(NYT, 'coco', function(data){
+  console.log(data);
+})
