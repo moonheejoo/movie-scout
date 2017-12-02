89ca596742979bf0d0de93f9965c616844b9940f
moonheejoo <jmoonmoon2@gmail.com>
2017-12-02 15:19:08 -0800

    render movie data

--- index.js
+++ index.js
@@ -1,86 +1,84 @@
+'use strict';
 
 const IMDB = {
   NAME: 'imdb',
   BASE_URL: 'http://www.theimdbapi.org/api/find/movie?'
-}
+};
 const NYT = {
   NAME: 'nyt',
-  API_KEY: "9b85bd3d754d4f91a251f25992575fdf",
-  BASE_URL: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?"
-}
+  BASE_URL: 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?',
+  KEY: '9b85bd3d754d4f91a251f25992575fdf'
+};
 
-
-// $.getJSON(NYT_SEARCH_URL, options);
-
-function getDataFromApi(search, callback) {
-  console.log("hello");
-  const query = {
-    title: `${search}`
-  }
-// getFromApi (IMDB, query, function (res) {
-//   console.log(res);
-// });
-
-  $.getJSON(IMDB.BASE_URL, query)
-    .done(callback)
-    .done(renderMovies)
-    .fail(function(error) {
-      console.log(error);
-    });
-  $.getJSON(NYT.BASE_URL, query)
-    .done(callback)
-    .done(renderMovies)
-    .fail(function(error) {
-      console.log(error);
-    })
+/**
+ * Initiates calls to the three unique APIs in our app.
+ * @param {string} query
+ */
+function initiateGetRequests(query) {
+  getFromApi(IMDB, query, function(data) {
+  renderImdb(data);
+    console.log(data);
+  });
+  getFromApi(NYT, query, function(data) {
+  renderNyt(data);
+  });
 }
 /**
-* @param {Object} api - the object of the API whose data we'll request
-* @param {string} api.NAME - A simple string which indicates the name of the api
-* @param {string} api.API_KEY - key to access an api
-* @param {string} api.BASE_URL - the base url onto which a query will be appended
-* @param {string} q - A query for the desired api
-* @param {function} cb - function to run after the initial get request
-*/
-function getFromApi(api, q, cb) {
-  let query;
-  console.log(api);
-  alert("getFromApi works")
-  if (api.NAME === 'nyt') {
+ * Makes a call to a single API.
+ * @param {Object} apiConfig - Information about an API
+ * @param {string} apiConfig.NAME
+ * @param {string} apiConfig.BASE_URL
+ * @param {string} [apiConfig.KEY] - Optional. Necessary for accessing some APIs.
+ * @param {string} query - A film or TV show title.
+ * @param {getJSONCb} handleData - Callback which processes the API data if the request is successful.
+ *
+ *
+ * @callback getJSONCb - Processes sussessful get requests.
+ * @param {Object} data - The data returned by the API.
+ */
+function getFromApi(apiConfig, query, handleData) {
+  let queryObj;
+  console.log(apiConfig);
+  if (apiConfig.NAME === 'nyt') {
     console.log('talking to the nyt!!');
-     query = {
-      'api-key': api.API_KEY,
-      'query': q,
-    }
-  if (api.NAME === 'imdb'){
-    console.log('talking to imdb');
-     query = {
-       'query': q,
-     }
-    }
+    queryObj = {
+      'api-key': apiConfig.KEY,
+      query: query
+    };
   }
-
-
-  $.getJSON(api.BASE_URL, query, cb);
+  if (apiConfig.NAME === 'imdb') {
+    console.log('talking to imdb');
+    queryObj = {
+      title: query
+    };
+  }
+  $.getJSON(apiConfig.BASE_URL, queryObj, handleData);
 }
 
-
 function cleanMovieData(data) {
-  const cleanedMovies = [data[0]]
+  const cleanedMovies = [data[0]];
   return cleanedMovies;
 }
 
-function renderMovies(data) {
+function renderImdb(data) {
   console.log(data);
   const imdbResults = `<div class="render-results-imdb">
     <h2>${data[0].title}</h2>
     <h3>${data[0].content_rating}</h3>
     <img src="${data[0].poster.thumb}">
     </div>
-  `
-  $(".js-search-results-imdb").html(imdbResults);
+  `;
+  $('.js-search-results-imdb').html(imdbResults);
 }
-  $(".js-search-results-nyt").html()
+
+function renderNyt(data) {
+  console.log(data.results);
+  const nytResults = `<div class="js-search-results-nyt">
+  <h2>${data.results[0].display_title}</h2></div>
+ `
+  console.log($("js-search-results-nyt"));
+  $('.js-search-results-nyt').html(nytResults);
+}
 
 
 $('.js-search-form').on('submit', event => {
@@ -88,10 +86,6 @@ $('.js-search-form').on('submit', event => {
 
   const queryTarget = $(event.currentTarget).find('.js-query');
   const query = queryTarget.val();
-  queryTarget.val("");
-  getDataFromApi(query, cleanMovieData);
+  queryTarget.val('');
+  initiateGetRequests(query);
 });
-
-//getFromApi(NYT, 'coco', function(data){
-//  console.log(data);
-// })
